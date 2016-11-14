f6e97919d5fbe7e21e396e306094fd9cfe6a92d1
Paco Martín Fernández <fmartinfdez@gmail.com>
2016-11-14 14:49:06 +0000

    Fixed bug dependencies

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from distutils.core import setup
-setup(
-  name = 'IBMQuantumExperience',
-  packages = ['IBMQuantumExperience'], # this must be the same as the name above
-  version = '0.1.1',
-  description='The SDK to use the IBM Quantum Experience Platform',
-  author='IBM Research Emergent Solutions',
-  author_email='fmartinfdez@gmail.com',
-  url = 'https://github.com/IBMResearch/python-sdk-quantum-experience',
-  keywords = ['ibm', 'quantum computer', 'quantum experience'],
-  license='MIT',
-  classifiers = [],
-)
+from setuptools import setup
+
+setup(name = 'IBMQuantumExperience',
+      packages = ['IBMQuantumExperience'], # this must be the same as the name above
+      version='0.1.2',
+      author='IBM Research Emergent Solutions',
+      author_email='fmartinfdez@gmail.com',
+      url = 'https://github.com/IBMResearch/python-sdk-quantum-experience',
+      keywords = ['ibm', 'quantum computer', 'quantum experience'],
+      license='MIT',
+      install_requires=[
+        'request',
+      ],
+      zip_safe=False)
